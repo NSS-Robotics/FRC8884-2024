da1d7bb3432c83463b78ebbe0a213f83cf85df66
Luan-Gil <luan.gil@student.tdsb.on.ca>
2024-02-20 16:22:46 -0500

    rename

--- FRC8884-2024/src/main/java/frc/robot/RobotContainer.java
+++ FRC8884-2024/src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.Swerve;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -34,7 +34,7 @@ public class RobotContainer {
 
     /* Driver Controller */
     private final XboxController m_driverController = new XboxController(OperatorConstants.kDriverControllerPort);
-    private final SwerveSubsystem m_exampleSubsystem = new SwerveSubsystem();
+    private final Swerve m_exampleSubsystem = new Swerve();
     JoystickButton in = new JoystickButton(m_driverController, XboxController.Button.kY.value);
     JoystickButton out = new JoystickButton(m_driverController, XboxController.Button.kA.value);
     // Replace with CommandPS4Controller or CommandJoystick if needed
