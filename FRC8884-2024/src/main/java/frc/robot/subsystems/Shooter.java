d57b2dc91b054698b74c0536e53215c3c52e8f7c
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2024-02-20 17:43:56 -0500

    static bad

--- FRC8884-2024/src/main/java/frc/robot/subsystems/Shooter.java
+++ FRC8884-2024/src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,25 +3,25 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Shooter {
-    private static TalonFX motor1;
-    private static TalonFX motor2;
+    private TalonFX motor1;
+    private TalonFX motor2;
 
     public Shooter(int motor1, int motor2) {
         this.motor1 = new TalonFX(motor1);
         this.motor2 = new TalonFX(motor2);
     }
 
-    public static void in() {
+    public void in() {
         motor1.set(-1);
         motor2.set(1);
     }
 
-    public static void out() {
+    public void out() {
         motor1.set(1);
         motor2.set(-1);
     }
 
-    public static void stop() {
+    public void stop() {
         motor1.set(0);
         motor2.set(0);
     }
