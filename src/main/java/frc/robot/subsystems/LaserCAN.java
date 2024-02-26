c4c77a9bcedbbbb977673b96beeb2cbd74c5f47c
James Begin <james.begin@student.tdsb.on.ca>
2024-02-26 10:30:13 -0500

    LaserCan Distance

--- src/main/java/frc/robot/subsystems/LaserCAN.java
+++ src/main/java/frc/robot/subsystems/LaserCAN.java
@@ -4,6 +4,8 @@ import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.ConfigurationFailedException;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class LaserCAN extends SubsystemBase {
     LaserCan lasercan = new LaserCan(Constants.LaserCanConstants.laserCan);
@@ -20,7 +22,8 @@ public class LaserCAN extends SubsystemBase {
 
     @Override
     public void periodic() {
-
+        LaserCan.Measurement m = lasercan.getMeasurement();
+        SmartDashboard.putNumber("LaserCan MM", m.distance_mm);
     }
 
 }
