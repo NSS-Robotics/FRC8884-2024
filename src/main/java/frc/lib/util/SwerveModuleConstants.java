4a2e561bdf2ee4dcba5189381120b399dca5adb7
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2024-03-02 16:10:05 -0500

    ,more formatt

--- src/main/java/frc/lib/util/SwerveModuleConstants.java
+++ src/main/java/frc/lib/util/SwerveModuleConstants.java
@@ -3,6 +3,7 @@ package frc.lib.util;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class SwerveModuleConstants {
+
     public final int driveMotorID;
     public final int angleMotorID;
     public final int cancoderID;
@@ -15,7 +16,12 @@ public class SwerveModuleConstants {
      * @param canCoderID
      * @param angleOffset
      */
-    public SwerveModuleConstants(int driveMotorID, int angleMotorID, int canCoderID, Rotation2d angleOffset) {
+    public SwerveModuleConstants(
+        int driveMotorID,
+        int angleMotorID,
+        int canCoderID,
+        Rotation2d angleOffset
+    ) {
         this.driveMotorID = driveMotorID;
         this.angleMotorID = angleMotorID;
         this.cancoderID = canCoderID;
