639a6548e2029b21dfcb1e998860e37c339538fe
Jacob <techsupport@kreindler.ca>
2024-03-04 22:51:48 -0500

    add voltage to dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,6 +50,8 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotPeriodic() {
+                SmartDashboard.putNumber("Battery Voltage", RobotController.getBatteryVoltage());
+
         // Runs the Scheduler. This is responsible for polling buttons, adding
         // newly-scheduled
         // commands, running already-scheduled commands, removing finished or
