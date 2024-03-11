dacd7160f490434be4294f6671c2c0cd8453ac32
Magnumic0ray <64858691+Magnumic0ray@users.noreply.github.com>
2024-03-11 15:44:49 -0400

    push?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -213,7 +213,7 @@ public final class Constants {
 
     public static final class AutoConstants {
 
-        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxSpeedMetersPerSecond = 2.5;
         public static final double kMaxAccelerationMetersPerSecondSquared = 3;
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared =
