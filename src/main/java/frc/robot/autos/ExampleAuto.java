f501776b59984a58e3859da0a9f0ad5f0ede5dca
Magnumic0ray <64858691+Magnumic0ray@users.noreply.github.com>
2024-03-11 09:51:34 -0400

    reverting

--- src/main/java/frc/robot/autos/ExampleAuto.java
+++ src/main/java/frc/robot/autos/ExampleAuto.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.Constants.AutoConstants;
