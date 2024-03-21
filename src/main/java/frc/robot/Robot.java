866b9a11ea6aaf5a887edde75f15a5e54d06f2ed
Shams C <105302032+ssc-red@users.noreply.github.com>
2024-03-20 23:37:19 -0400

    candle fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -120,6 +120,7 @@ public class Robot extends TimedRobot {
         }
         m_robotContainer.s_swerve.zeroGyro();
         m_robotContainer.s_swerve.setLimelightStatus(true);
+        m_robotContainer.l_candle.reset();
     }
 
     /** This function is called periodically during operator control. */
